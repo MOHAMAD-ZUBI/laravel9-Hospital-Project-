14f5b4bec5b3ee0b4c3e56153249351228fb913a
MOHAMAD-ZUBI <2010205590@ogrenci.karabuk.edu.tr>
2022-04-13 03:54:28 +0300

    created table: show,delete,edit

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -14,6 +14,56 @@
                     <h5>Welcome Jhon Deo , Love to see you back. </h5>
 
                 </div>
+                <div class="col-md-6">
+                    <div class="box">
+                        <div class="box-header">
+                            <h3 class="box-title">Category List</h3>
+                        </div><!-- /.box-header -->
+                        <div class="box-body">
+                            <table class="table table-bordered">
+                                <tbody><tr>
+                                    <th style="width: 10px">Id</th>
+                                    <th>Title</th>
+                                    <th>Keywords</th>
+                                    <th>Description</th>
+                                    <th>Image</th>
+                                    <th>Status</th>
+                                    <th style="width: 40px">Edit</th>
+                                    <th style="width: 40px">Delete</th>
+                                    <th style="width: 40px">Show</th>
+
+                                </tr>
+
+                                @foreach($data as $rs)
+                                <tr>
+                                    <td>{{$rs->id}}</td>
+                                    <td>{{$rs->title}}</td>
+                                    <td>{{$rs->keywords}}</td>
+                                    <td>{{$rs->description}}</td>
+                                    <td>{{$rs->image}}</td>
+                                    <td>{{$rs->status}}</td>
+                                    <td><a her="/admin/category/edit{{$rs->status}}" class="btn btn-block btn-info btn-sm">Edit</a> </td>
+                                    <td><a her="/admin/category/delete{{$rs->status}}"class="btn btn-block btn-danger btn-sm">Delete</a></td>
+                                    <td><a her="/admin/category/show{{$rs->status}}"class="btn btn-block btn-success btn-sm">Show</a></td>
+
+                                </tr>
+
+                                @endforeach
+                                </tbody>
+
+                            </table>
+                        </div><!-- /.box-body -->
+                        <div class="box-footer clearfix">
+                            <ul class="pagination pagination-sm no-margin pull-right">
+                                <li><a href="#">«</a></li>
+                                <li><a href="#">1</a></li>
+                                <li><a href="#">2</a></li>
+                                <li><a href="#">3</a></li>
+                                <li><a href="#">»</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
             <!-- /. ROW  -->
             <hr />
