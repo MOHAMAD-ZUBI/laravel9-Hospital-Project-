fe3953e469fadd9193463d93283a96be6e6a0869
MOHAMAD-ZUBI <2010205590@ogrenci.karabuk.edu.tr>
2022-06-12 22:21:35 +0300

    Appointment home & Admin 0.1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,7 +24,7 @@ class HomeController extends Controller
     public function index()
     {
         $page ='home';
-        $sliderdata=Policlinic::limit(4)->get();
+        $sliderdata=Category::limit(4)->get();
         $policliniclist1=Policlinic::limit(6)->get();
         $setting = Setting::first();
         return view('home.index',[
--- resources/views/home/contactus.blade.php
+++ resources/views/home/contactus.blade.php
@@ -52,4 +52,25 @@
             </div>
         </div>
     </section>
+    <section class="about_section layout_padding">
+        <div class="container  ">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="detail-box">
+                        <div class="heading_container">
+                            <h2>
+                                Contact <span>Info</span>
+                            </h2>
+                        </div>
+                        <p>
+                            {!! $setting->contact !!}
+                        </p>
+                        <a href="\">
+                            Go Back
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
